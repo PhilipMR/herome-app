7a0b977667918a7ace2d89956da0a312aa4973eb
Philip Roozendaal <p.m.roozendaal@student.tudelft.nl>
2018-11-01 16:20:03 +0100

    Add empty click events for login screen elements

--- HeromeApp/HeromeApp/HeromeApp/LoginPage.xaml.cs
+++ HeromeApp/HeromeApp/HeromeApp/LoginPage.xaml.cs
@@ -8,11 +8,47 @@ namespace HeromeApp
         public LoginPage()
         {
             InitializeComponent();
-        }
 
-        private void LoginButton_Clicked(object sender, EventArgs e)
+			this.imgFacebookIcon.GestureRecognizers.Add(new TapGestureRecognizer
+			{
+				Command = new Command(() => LoginFacebook_Clicked()),
+				NumberOfTapsRequired = 1
+			});
+			this.imgTwitterIcon.GestureRecognizers.Add(new TapGestureRecognizer
+			{
+				Command = new Command(() => LoginTwitter_Clicked()),
+				NumberOfTapsRequired = 1
+			});
+		}
+
+		private void LoginButton_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new HomePage());
-        }
-    }
+			Console.WriteLine("Login button clicked!");
+		}
+
+		private void RegisterButton_Clicked(object sender, EventArgs e)
+		{
+			Console.WriteLine("Register button clicked!");
+		}
+
+		private void ForgotPassword_Clicked(object sender, EventArgs e)
+		{
+			Console.WriteLine("Forgot password button clicked!");
+		}
+
+		private void LoginFacebook_Clicked()
+		{
+			Console.WriteLine("Login with facebook image clicked!");
+		}
+
+		private void LoginTwitter_Clicked()
+		{
+			Console.WriteLine("Login with twitter image clicked!");
+		}
+
+		private void SkipLogin_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new HomePage());
+		}
+	}
 }
