18b715933826f8e7dba31cfbc7bfb6ac1e6a48e6
Philip Roozendaal <p.m.roozendaal@student.tudelft.nl>
2018-11-19 14:25:58 +0100

    Add click listeners for the home page's panels and create basic layout for nails page

--- HeromeApp/HeromeApp/HeromeApp/HomePage.xaml.cs
+++ HeromeApp/HeromeApp/HeromeApp/HomePage.xaml.cs
@@ -6,9 +6,50 @@ namespace HeromeApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HomePage : ContentPage
 	{
+		#region Constructors
 		public HomePage ()
 		{
 			InitializeComponent ();
+			this.imgNailsPanel.GestureRecognizers.Add(new TapGestureRecognizer
+			{
+				Command = new Command(() => NailsPanel_Clicked()),
+				NumberOfTapsRequired = 1
+			});
+			this.imgHandsPanel.GestureRecognizers.Add(new TapGestureRecognizer
+			{
+				Command = new Command(() => HandsPanel_Clicked()),
+				NumberOfTapsRequired = 1
+			});
+			this.imgCuticlesPanel.GestureRecognizers.Add(new TapGestureRecognizer
+			{
+				Command = new Command(() => CuticlesPanel_Clicked()),
+				NumberOfTapsRequired = 1
+			});
+			this.imgPolishPanel.GestureRecognizers.Add(new TapGestureRecognizer
+			{
+				Command = new Command(() => PolishPanel_Clicked()),
+				NumberOfTapsRequired = 1
+			});
 		}
-    }
+		#endregion
+
+		#region Control events
+		private void NailsPanel_Clicked()
+		{
+			Navigation.PushAsync(new NailsPage());
+		}
+
+		private void HandsPanel_Clicked()
+		{
+		}
+
+		private void CuticlesPanel_Clicked()
+		{
+		}
+
+		private void PolishPanel_Clicked()
+		{
+		}
+		#endregion
+	}
 }
