a4810b4ef35a4c7d86452e9df9dc5aac8eb6f7f1
Philip Roozendaal <p.m.roozendaal@student.tudelft.nl>
2018-11-30 14:23:11 +0100

    Add a nails page with a modelview based questionaire (not finished)

---
+++ HeromeApp/HeromeApp/HeromeApp/Models/QuestionaireViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace HeromeApp.Models
+{
+	public class QuestionaireViewModel : INotifyPropertyChanged
+	{
+		#region Question model
+		public class Question
+		{
+			public string Caption { get; set; }
+			public List<string> Options { get; set; }
+		}
+		#endregion
+
+		#region Result model
+		public class Result
+		{
+			public List<int> Options { get; set; }
+			public string Url { get; set; }
+		}
+		#endregion
+
+		#region Events
+		public event PropertyChangedEventHandler PropertyChanged;
+		#endregion
+
+		#region Properties and fields
+		public ObservableCollection<Question> Questions { get; }
+		public ObservableCollection<Result> Results { get; }
+		public Question ActiveQuestion { get; }
+		#endregion
+
+		#region Constructors
+		public QuestionaireViewModel(List<Question> questions, List<Result> results)
+		{
+			if (questions.Count == 0)
+			{
+				throw new ArgumentException("The questions list contains no elements!");
+			}
+			if (results.Count == 0)
+			{
+				throw new ArgumentException("The results list contains no elements!");
+			}
+			this.Questions = new ObservableCollection<Question>(questions);
+			this.Results = new ObservableCollection<Result>(results);
+
+			this.ActiveQuestion = Questions[0];
+			this.Questions.CollectionChanged += Questions_CollectionChanged;
+			this.Results.CollectionChanged += Results_CollectionChanged;
+		}
+		#endregion
+
+		#region Control events
+		private void Questions_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Questions)));
+		}
+		private void Results_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Results)));
+		}
+		#endregion
+	}
+}
--- HeromeApp/HeromeApp/HeromeApp/NailsPage.xaml.cs
+++ HeromeApp/HeromeApp/HeromeApp/NailsPage.xaml.cs
@@ -1,20 +1,56 @@
-﻿using System;
+﻿using HeromeApp.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static HeromeApp.Models.QuestionaireViewModel;
 
 namespace HeromeApp
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NailsPage : ContentPage
 	{
-		public NailsPage ()
+		public NailsPage()
 		{
-			InitializeComponent ();
+			InitializeComponent();
+
+			List<Question> questions = new List<Question>
+			{
+				new Question()
+				{
+					Caption = "Ik heb last van...",
+					Options = new List<string>
+					{
+						"vastzittende nagelriemen",
+						"droge nagelriemen",
+						"nagelriemvelletjes"
+					}
+				},
+				new Question()
+				{
+					Caption = "En ik heb ook nog eens last van...",
+					Options = new List<string>
+					{
+						"het weer",
+						"iedereen",
+						"alles",
+						"niks"
+					}
+				}
+			};
+
+			List<Result> results = new List<Result>
+			{
+				new Result()
+				{
+					Options = new List<int>
+					{
+						0
+					},
+					Url = "https://herome.nl/xyz"
+				}
+			};
+
+			this.BindingContext = new QuestionaireViewModel(questions, results);
 		}
 	}
 }
